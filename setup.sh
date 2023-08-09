42b9e0ac37ba3d0c97793bc2269e0ff1288c7404
Mastermind-U <rex49513@gmail.com>
2023-08-09 14:56:55 +0300

    refactor: script improvements

--- setup.sh
+++ setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 touch .env
-read -p "Enter postgres user [default: user]: " postgres_user
-postgres_user=${postgres_user:-user}
+> .env
+read -p "Enter postgres user [default: pg-user]: " postgres_user
+postgres_user=${postgres_user:-pg-user}
 echo "POSTGRES_USER="$postgres_user >> .env
 
 read -p "Enter postgres host (leave it default if you are using default database) [default: postgres]: " postgres_host
@@ -15,12 +16,13 @@ echo "POSTGRES_PASSWORD="$postgres_password >> .env
 read -p "Enter api domain [required]: " api_domain
 echo "API_DOMAIN="$api_domain >> .env
 
-if [ -z "$api_domain" ]; then echo "api domain required" && exit 1; fi
+if [ -z "$api_domain" ]; then echo "api domain required" && > .env && exit 1; fi
 
 read -p "Enter interface domain [required]: " int_domain
 echo "DOMAIN="$int_domain >> .env
 
 if [ -z "$int_domain" ]; then echo "interface domain required" && exit 1; fi
+if [[ "$int_domain" == "$api_domain" ]]; then echo "cannot host api and interface on one domain" && > .env && exit 1; fi
 
 secret_key=$(openssl rand -hex 32)
 echo "SECRET_KEY="$secret_key >> .env
