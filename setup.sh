83c2f9d0ca940ca15f40d78244f10a2a6eafda07
Mastermind-U <rex49513@gmail.com>
2023-08-09 20:30:55 +0300

    refactor: bulk write

--- setup.sh
+++ setup.sh
@@ -1,28 +1,32 @@
 #!/bin/bash
 touch .env
 > .env
+
 read -p "Enter postgres user [default: user]: " postgres_user
 postgres_user=${postgres_user:-user}
-echo "POSTGRES_USER="$postgres_user >> .env
+
 
 read -p "Enter postgres host (leave it default if you are using default database) [default: postgres]: " postgres_host
 postgres_host=${postgres_host:-postgres}
-echo "POSTGRES_HOST="$postgres_host >> .env
+
 
 read -p "Enter postgres password [default: autogenerate]: " postgres_password
 postgres_password=${postgres_password:-$(openssl rand -hex 16)}
-echo "POSTGRES_PASSWORD="$postgres_password >> .env
+
 
 read -p "Enter api domain [required]: " api_domain
-echo "API_DOMAIN="$api_domain >> .env
-
 if [ -z "$api_domain" ]; then echo "api domain required" && > .env && exit 1; fi
 
-read -p "Enter interface domain [required]: " int_domain
-echo "DOMAIN="$int_domain >> .env
 
+read -p "Enter interface domain [required]: " int_domain
 if [ -z "$int_domain" ]; then echo "interface domain required" && exit 1; fi
 if [[ "$int_domain" == "$api_domain" ]]; then echo "cannot host api and interface on one domain" && > .env && exit 1; fi
 
 secret_key=$(openssl rand -hex 32)
+
+echo "POSTGRES_HOST="$postgres_host >> .env
+echo "POSTGRES_USER="$postgres_user >> .env
+echo "POSTGRES_PASSWORD="$postgres_password >> .env
+echo "API_DOMAIN="$api_domain >> .env
+echo "DOMAIN="$int_domain >> .env
 echo "SECRET_KEY="$secret_key >> .env
