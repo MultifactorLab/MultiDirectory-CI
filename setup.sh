88df79c1152f347b9fd3c794e364f4fae8cc5111
Mastermind-U <rex49513@gmail.com>
2023-08-09 21:32:01 +0300

    refactor: single domain service

--- setup.sh
+++ setup.sh
@@ -14,19 +14,13 @@ read -p "Enter postgres password [default: autogenerate]: " postgres_password
 postgres_password=${postgres_password:-$(openssl rand -hex 16)}
 
 
-read -p "Enter api domain [required]: " api_domain
-if [ -z "$api_domain" ]; then echo "api domain required" && > .env && exit 1; fi
-
-
-read -p "Enter interface domain [required]: " int_domain
-if [ -z "$int_domain" ]; then echo "interface domain required" && exit 1; fi
-if [[ "$int_domain" == "$api_domain" ]]; then echo "cannot host api and interface on one domain" && > .env && exit 1; fi
+read -p "Enter interface domain [required]: " domain
+if [ -z "$domain" ]; then echo "interface domain required" && exit 1; fi
 
 secret_key=$(openssl rand -hex 32)
 
 echo "POSTGRES_HOST="$postgres_host >> .env
 echo "POSTGRES_USER="$postgres_user >> .env
 echo "POSTGRES_PASSWORD="$postgres_password >> .env
-echo "API_DOMAIN="$api_domain >> .env
-echo "DOMAIN="$int_domain >> .env
+echo "DOMAIN="$domain >> .env
 echo "SECRET_KEY="$secret_key >> .env
